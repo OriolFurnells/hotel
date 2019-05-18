e7242079036942f1aac6869ffb2f7dbea64dc2d4
Oriol Furnells <45357022+OriolFurnells@users.noreply.github.com>
2019-05-18 13:13:12 +0200

    componentsCreate

---
+++ hotel-master/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TablaComponent } from './Componentes/tabla/tabla/tabla.component';
+import { FormComponent } from './Componentes/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablaComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
